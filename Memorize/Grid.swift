e28201e69915e5e53f6ea53d00928b95d4ca5bc2
RadenAR <radenabdulrahman@gmail.com>
2020-06-30 15:55:17 -0400

    created a file for the grid uiview

---
+++ Memorize/Grid.swift
@@ -0,0 +1,15 @@
+//
+//  Grid.swift
+//  Memorize
+//
+//  Created by Raden Abdul Rahman on 6/30/20.
+//  Copyright © 2020 Raden. All rights reserved.
+//
+
+import SwiftUI
+
+struct Grid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
