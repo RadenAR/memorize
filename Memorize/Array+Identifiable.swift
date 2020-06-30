ef00f8f7b57483889622677b8ab45c645ae42603
RadenAR <radenabdulrahman@gmail.com>
2020-06-30 17:55:02 -0400

    factoring out array extender to its own file

---
+++ Memorize/Array+Identifiable.swift
@@ -0,0 +1,20 @@
+//
+//  Array+Identifiable.swift
+//  Memorize
+//
+//  Created by Raden Abdul Rahman on 6/30/20.
+//  Copyright © 2020 Raden. All rights reserved.
+//
+
+import Foundation
+
+extension Array where Element: Identifiable {
+    func firstIndex(matching: Element) -> Int {
+        for index in 0..<self.count {
+            if self[index].id == matching.id {
+                return index
+            }
+        }
+        return 0 //TODO: Bogus
+    }
+}
