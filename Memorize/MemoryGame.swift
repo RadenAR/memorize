cf92f9f496c94528331447a617adb4532a9215ad
RadenAR <radenabdulrahman@gmail.com>
2020-06-30 20:21:30 -0400

    matched cards logic implemented however flips down still after matching

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -10,13 +10,27 @@ import Foundation
 
 // we add cardcontent as it needs to be specified like when using an array and below in the card struct we use that type. insert String or Int etc
 // cardcontent is our "dont care type"
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     var cards: Array<Card>
+    
+    var indexOfTheOneAndOnlyFaceUpCard: Int?
     
     mutating func choose(card: Card) {
         print("card chosen: \(card)")
-        if let chosenIndex = cards.firstIndex(matching: card) {
-            cards[chosenIndex].isFaceUp = !cards[chosenIndex].isFaceUp
+        if let chosenIndex = cards.firstIndex(matching: card), !cards[chosenIndex].isFaceUp, !cards[chosenIndex].isMatched {
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                indexOfTheOneAndOnlyFaceUpCard = nil
+            } else {
+                for index in cards.indices {
+                    cards[index].isFaceUp = false
+                }
+                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+            }
+            self.cards[chosenIndex].isFaceUp = true
         }
     }
     
@@ -36,7 +50,7 @@ struct MemoryGame<CardContent> {
     struct Card: Identifiable {
         var id: Int
         // below we give an initialized dafault value
-        var isFaceUp: Bool = true
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
     }
